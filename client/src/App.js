6f3f9cbbf1774f40a86316086065562701199b50
Yves Cédric <ryc.devweb@gmail.com>
2022-03-10 10:42:23 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <nav>Nav</nav>
+      <main>
+        <header>
+          <a href="/">School Management</a>
+        </header>
+        <div>Content</div>
+      </main>
+    </>
+  );
+};
+
+export default App;
